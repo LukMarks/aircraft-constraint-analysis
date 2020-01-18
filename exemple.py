6ed602dfced03832664146a8f7f38aefa2da5fc7
LukMarks <lucas.mmoreno@hotmail.com>
2020-01-18 14:26:09 -0300

    First commit files

---
+++ exemple.py
@@ -0,0 +1,8 @@
+# Author: Lucas Marques Moreno
+# Date: 01/18/2020 
+# description: Exemple for a 
+# simple contraint analysis
+
+import numpy as np
+import matplotlib.pyplot as plt
+import contraint_analysis
