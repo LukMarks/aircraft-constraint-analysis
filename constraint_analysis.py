14c07ecc6af734ee7c077dfdda601061a86b2056
LukMarks <lucas.mmoreno@hotmail.com>
2020-01-18 14:38:38 -0300

    Add the constant-velocity turn function

--- constraint_analysis.py
+++ constraint_analysis.py
@@ -8,9 +8,19 @@ class design_space:
         pass
         return
 
-    def constant_velocity_turn(self):
-        pass
-        return
+    def constant_velocity_turn(self): # T/W for a level constante-velocity turn
+        # Variables:
+        # CD_min: minimun drag coefficent
+        # k: lift-induced drag constant
+        # q[N/m²]: dynamic pressure at selected speed and altitude
+        # S[m²] : wing area
+        # T[N]: Thrust
+        # W[N]: weight
+        # n: load factor or 1/cos(psi), where psi is the bank angle
+        aux1 = CD_min/w_s
+        aux2 = k*(n/q)**2
+        t_w = q*(aux1*aux2*w_s)
+        return t_w
     
     def specifc_energy_level(self):
         pass
