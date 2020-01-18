084bd4278ef327e9fa65a41e5f04fa71fd7123b5
LukMarks <lucas.mmoreno@hotmail.com>
2020-01-18 14:30:46 -0300

    Add initial structure for analysis

---
+++ constraint_analysis.py
@@ -0,0 +1,28 @@
+# Author: Lucas Marques Moreno
+# Date: 01/18/2020 
+# description: Group of function
+# to define the design space
+
+class design_space:
+    def __init__(self):
+        pass
+        return
+
+    def constant_velocity_turn(self):
+        pass
+        return
+    
+    def specifc_energy_level(self):
+        pass
+        return
+
+    def takeoff_distance(self):
+        pass
+        return
+    
+    def cruise_airspeed(self):
+        pass
+        return
+    def service_ceiling(self):
+        pass
+        return
